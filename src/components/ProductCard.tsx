24872f4ce27e25e5cadeb9ea16d4e2e478ac4cae
Joe <145125821+TxNixon@users.noreply.github.com>
2025-01-28 04:26:56 +0700

    create product page

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router"; // Correct import from react-router-dom
 import styles from "./ProductCard.module.css";
 
 type ProductCardProps = {
@@ -9,8 +10,14 @@ type ProductCardProps = {
 };
 
 const ProductCard = ({ product }: ProductCardProps) => {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/produk/${product.name}`);
+  };
+
   return (
-    <div className={styles.productCard}>
+    <div onClick={handleCardClick} className={styles.productCard}>
       <img src={product.image} alt={product.name} />
       <p>{product.price}</p>
       <h2>{product.name}</h2>
--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,12 +1,10 @@
-import { Link, useParams } from "react-router";
+import { useParams } from "react-router";
 const ProductDetailPage = () => {
   const params = useParams();
   return (
     <div>
       <h1>Ini Product Detail Page</h1>
-      <h3>{params.produkName}</h3>
-
-      <Link to="/">Kembali ke Home</Link>
+      <h2>{params.produkName}</h2>
     </div>
   );
 };
